4aca329696a03854364c44387945e9a3e4ce6ecf
Vasko1234 <vasilvelev07@gmail.com>
2022-06-23 23:54:37 +0300

    100% done

---
+++ script2.js
@@ -0,0 +1,127 @@
+var button1 = document.getElementById("btn1");
+var button2 = document.getElementById("btn2");
+var button3 = document.getElementById("btn3");
+var button4 = document.getElementById("btn4");
+var button5 = document.getElementById("btn5");
+var button6 = document.getElementById("btn6");
+var button7 = document.getElementById("btn7");
+var button8 = document.getElementById("btn8");
+var button9 = document.getElementById("btn9");
+
+
+button1.addEventListener("mouseover", () => {
+    button1.style.color = 'black';
+    button1.style.backgroundColor = 'grey';
+    button1.style.borderRadius = 1 + 'vmin';
+    button1.style.cursor = 'pointer';
+});
+button1.addEventListener("mouseout", () => {
+    button1.style.color = 'white';
+    button1.style.backgroundColor = '#363739';
+    button1.style.borderRadius = 0;
+    button1.style.cursor = 'default';
+});
+
+button2.addEventListener("mouseover", () => {
+    button2.style.color = 'black';
+    button2.style.backgroundColor = 'grey';
+    button2.style.borderRadius = 1 + 'vmin';
+    button2.style.cursor = 'pointer';
+});
+button2.addEventListener("mouseout", () => {
+    button2.style.color = 'white';
+    button2.style.backgroundColor = '#363739';
+    button2.style.borderRadius = 0;
+    button2.style.cursor = 'default';
+});
+
+button3.addEventListener("mouseover", () => {
+    button3.style.color = 'black';
+    button3.style.backgroundColor = 'grey';
+    button3.style.borderRadius = 1 + 'vmin';
+    button3.style.cursor = 'pointer';
+});
+button3.addEventListener("mouseout", () => {
+    button3.style.color = 'white';
+    button3.style.backgroundColor = '#363739';
+    button3.style.borderRadius = 0;
+    button3.style.cursor = 'default';
+});
+
+button4.addEventListener("mouseover", () => {
+    button4.style.color = 'black';
+    button4.style.backgroundColor = 'grey';
+    button4.style.borderRadius = 1 + 'vmin';
+    button4.style.cursor = 'pointer';
+});
+button4.addEventListener("mouseout", () => {
+    button4.style.color = 'white';
+    button4.style.backgroundColor = '#363739';
+    button4.style.borderRadius = 0;
+    button4.style.cursor = 'default';
+});
+
+button5.addEventListener("mouseover", () => {
+    button5.style.color = 'black';
+    button5.style.backgroundColor = 'grey';
+    button5.style.borderRadius = 1 + 'vmin';
+    button5.style.cursor = 'pointer';
+});
+button5.addEventListener("mouseout", () => {
+    button5.style.color = 'white';
+    button5.style.backgroundColor = '#363739';
+    button5.style.borderRadius = 0;
+    button5.style.cursor = 'default';
+});
+
+button6.addEventListener("mouseover", () => {
+    button6.style.color = 'black';
+    button6.style.backgroundColor = 'grey';
+    button6.style.borderRadius = 1 + 'vmin';
+    button6.style.cursor = 'pointer';
+});
+button6.addEventListener("mouseout", () => {
+    button6.style.color = 'white';
+    button6.style.backgroundColor = '#363739';
+    button6.style.borderRadius = 0;
+    button6.style.cursor = 'default';
+});
+
+button7.addEventListener("mouseover", () => {
+    button7.style.color = 'black';
+    button7.style.backgroundColor = 'grey';
+    button7.style.borderRadius = 1 + 'vmin';
+    button7.style.cursor = 'pointer';
+});
+button7.addEventListener("mouseout", () => {
+    button7.style.color = 'white';
+    button7.style.backgroundColor = '#363739';
+    button7.style.borderRadius = 0;
+    button7.style.cursor = 'default';
+});
+
+button8.addEventListener("mouseover", () => {
+    button8.style.color = 'black';
+    button8.style.backgroundColor = 'grey';
+    button8.style.borderRadius = 1 + 'vmin';
+    button8.style.cursor = 'pointer';
+});
+button8.addEventListener("mouseout", () => {
+    button8.style.color = 'white';
+    button8.style.backgroundColor = '#363739';
+    button8.style.borderRadius = 0;
+    button8.style.cursor = 'default';
+});
+
+button9.addEventListener("mouseover", () => {
+    button9.style.color = 'black';
+    button9.style.backgroundColor = 'grey';
+    button9.style.borderRadius = 1 + 'vmin';
+    button9.style.cursor = 'pointer';
+});
+button9.addEventListener("mouseout", () => {
+    button9.style.color = 'white';
+    button9.style.backgroundColor = '#363739';
+    button9.style.borderRadius = 0;
+    button9.style.cursor = 'default';
+});
